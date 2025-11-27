d58ce4d9a83ba53e2123101e36a0ee9f73dbda8a
moussar <moussar@mosh.razzouki.org>
2025-11-27 22:35:12 +0100

    add kanagawa

--- init.lua
+++ init.lua
@@ -881,11 +881,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('kanagawa').setup {
+        theme = 'dragon', -- wave, dragon, lotus
+        -- transparant = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -893,8 +895,7 @@ require('lazy').setup({
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
@@ -1011,6 +1012,9 @@ require('lazy').setup({
     },
   },
 })
+
+-- Set the Kanagawa colorscheme after all plugins are loaded
+vim.cmd.colorscheme 'kanagawa'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
